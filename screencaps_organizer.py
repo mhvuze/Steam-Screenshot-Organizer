30fa5a55c52f42110681152f9634b4a22fb1b39f
B. Kitty Elliott <GlitchyCat@users.noreply.github.com>
2016-05-14 18:10:04 -0700

    implement saving/loading steam app ID's and names to/from file. Rename functions with more descriptive names

--- screencaps_organizer.py
+++ screencaps_organizer.py
@@ -5,12 +5,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def steam():
-    def get_app_name(app_id):
-        print 'retrieving name for steam app', app_id
+def organize_steam_screencaps():
+    def get_app_name(steam_app_id):
+        print 'retrieving name for steam app', steam_app_id
         try:
             soup = BeautifulSoup(
-                requests.get('https://steamdb.info/app/' + app_id)
+                requests.get('https://steamdb.info/app/' + steam_app_id)
                     .content, 'html.parser')
             name = soup.find('td', itemprop='name').string
 
@@ -20,15 +20,24 @@ def steam():
 
             return name
         except requests.exceptions.RequestException:
-            print 'error obtaining name for', app_id
+            print 'error obtaining name for', steam_app_id
             return None
 
     pattern = re.compile(r'^(\d{6})_(\d{14}|\d{4}-\d{2}-\d{2})_(\d+)\.png$')
 
     path = r'C:\Users\Blake\Pictures\screencaps\Steam'
 
-    # TODO: load app IDs and Names from file, if one exists
     app_names = {}
+    # load app IDs and Names from file, if one exists
+    names_storage_path = os.path.join(path, 'app names dictionary.txt')
+    if os.path.exists(names_storage_path):
+        names_storage_pattern = re.compile(r'^(.+)  :\|:  (.+)$')
+        for line in open(names_storage_path, 'r'):
+            match = re.match(names_storage_pattern, line)
+            if match:
+                app_names[match.group(1)] = match.group(2)
+        del names_storage_pattern
+        # print app_names
 
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
@@ -41,7 +50,6 @@ def steam():
         if match:
             app_id = match.group(1)
 
-            # TODO: save app IDs and Names to file
             if app_id not in app_names:
                 app_names[app_id] = get_app_name(app_id)
 
@@ -55,10 +63,14 @@ def steam():
 
             os.renames(file_path, new_path)
 
-            # print new_path
+    # save app IDs and Names to file
+    names_storage_file = open(names_storage_path, 'w')
+    for key in app_names:
+        names_storage_file.write(key + '  :|:  ' + app_names[key] + '\n')
+    names_storage_file.close()
 
 
-def VLC():
+def organize_vlc_screencaps():
     pattern = re.compile(r'^(.+)-(\d{5}).png$')
 
     path = r'C:\Users\Blake\Pictures\screencaps\VLC'
@@ -104,7 +116,7 @@ def clean_empty_directories(path):
 
 
 if __name__ == '__main__':
-    steam()
-    VLC()
+    organize_steam_screencaps()
+    organize_vlc_screencaps()
     print 'pruning screencaps folder for empty directories'
     clean_empty_directories(r'C:\Users\Blake\Pictures\screencaps\16')
